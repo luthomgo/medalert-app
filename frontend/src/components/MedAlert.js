4488d133c17eb0d7c9e05e8946439d88d13e0be0
lmgolombane023 <lmgolombane023@student.wethinkcode.co.za>
2025-05-22 20:27:10 +0200

    first commit

---
+++ frontend/src/components/MedAlert.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import MedAlert from './components/MedAlert';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MedAlert />
+    </div>
+  );
+}
+
+export default App;
